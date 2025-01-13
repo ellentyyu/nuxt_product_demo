2264796199f3dcfb45b3a3ea7d5d9f838e891a2c
ellen <ellentyyu@gmail.com>
2025-01-13 21:12:56 +0800

    update: copy api data to public folder

---
+++ public/categoryData.js
@@ -0,0 +1,48 @@
+export default defineEventHandler((event) => {
+    return [
+        {
+            "id": "all",
+            "name": "最新"
+        },
+        {
+            "id": "190",
+            "name": "生活",
+            "description": "",
+            "image": "",
+            "image_mobile": "",
+            "is_free": "0",
+            "for_sort": "1",
+            "product_count": "160"
+        },
+        {
+            "id": "169",
+            "name": "美食",
+            "description": "美食相關的試用品",
+            "image": "",
+            "image_mobile": "",
+            "is_free": "0",
+            "for_sort": "2",
+            "product_count": "73"
+        },
+        {
+            "id": "172",
+            "name": "圖書",
+            "description": "",
+            "image": "",
+            "image_mobile": "",
+            "is_free": "0",
+            "for_sort": "3",
+            "product_count": "35"
+        },
+        {
+            "id": "193",
+            "name": "影視",
+            "description": "影視試用區",
+            "image": "",
+            "image_mobile": "",
+            "is_free": "0",
+            "for_sort": "4",
+            "product_count": "28"
+        }
+    ];
+});
